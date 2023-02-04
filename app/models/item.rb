88571dd376f41b0e0993125a464d52cca5e80206
Raphaelle PEREZ HORNBORG <raphaelleph@gmail.com>
2023-02-04 15:10:18 +0100

    added validations for request

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_many :requests
+
+  has_one_attached :photo
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,7 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+  validates :needed_item, presence: true
+  validates :category, presence: true
 end
