53026c4db02e79beb08a94a07999aacd6fff718a
Raphaelle PEREZ HORNBORG <raphaelleph@gmail.com>
2023-02-04 15:44:11 +0100

    Added validations for Item and Request

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,7 +1,8 @@
 class Request < ApplicationRecord
   belongs_to :user
-  belongs_to :item
+  belongs_to :item, optional: true
 
   validates :needed_item, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: ["Hygiène", "Médical", "Petit électroménager", "Puériculture"],
+  message: "%{value} n'est pas une catégorie valide. Veuillez choisir dans la liste proposée" }
 end
