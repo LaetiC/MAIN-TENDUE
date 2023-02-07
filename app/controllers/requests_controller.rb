c71f239659c458b772c80a8e79b4df60e3fe6f57
Laetitia Commanay <lmc2266@columbia.edu>
2023-02-07 21:37:42 +0100

    requests new create update

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,25 +1,37 @@
 class RequestsController < ApplicationController
   def new
+    @request = Request.new
   end
 
   def create
+    @request = Request.new(request_params)
+    if @request.item? == true
+      @request.status = "confirmed"
+    else
+      @request.status = "pending"
+    end
+    @request.item = @item
+    @request.user = current_user
+    @request.save
+    redirect_to request_path(@request)
   end
 
   def update
+    set_request
+    if @request.item? == true
+      @request.status = "confirmed"
+      @request.save
+    end
+    redirect_to dashboard_path
   end
 
   def edit
   end
 
-  def destroy
+private
+
+  def request_params
+    params.require(:request).permit(:user_id, :status, :category, :needed_item, :item_id)
   end
 
-  def index
-  end
-
-  def show
-  end
-
-  def confirmation
-  end
 end
