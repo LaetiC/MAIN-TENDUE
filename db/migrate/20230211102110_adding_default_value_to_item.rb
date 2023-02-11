570f8982d204716fe975962980a5f50755c3b278
Adrien De Tchaguine <adriendetchaguine@gmail.com>
2023-02-11 17:27:03 +0100

    changing default status migrations to Fr

--- db/migrate/20230211102110_adding_default_value_to_item.rb
+++ db/migrate/20230211102110_adding_default_value_to_item.rb
@@ -1,5 +1,5 @@
 class AddingDefaultValueToItem < ActiveRecord::Migration[7.0]
   def change
-    change_column :items, :status, :text, default: "created"
+    change_column :items, :status, :text, default: "Objet trouvé"
   end
 end
