63bfe9690c44d82fc6e9a746849efcc5c5f899c4
Raphaelle PEREZ HORNBORG <raphaelleph@gmail.com>
2023-02-23 20:59:54 +0100

    flatpickr conditional ok without margaux's card JS

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -3,47 +3,42 @@ import flatpickr from "flatpickr";
 
 export default class extends Controller {
   static targets = [ 'pickup_date', 'pickup_type' ]
+
   update() {
-    console.log(this.pickup_typeTarget)
-  }
-  // faire la fonction if avec le resultat de la fonction update qui ecoute le changement de valeur de Pickup_target. 
-  connect() {
-    flatpickr(this.pickup_dateTarget, {
-        enableTime: true,
-        time_24hr: true,
-        minTime: "11:00",
-        maxTime: "20:00",
-        altInput: true,
-        altFormat: "F j, Y",
-        dateFormat: "Y-m-d H:i",
-        minDate: "today",
-        maxDate: new Date().fp_incr(7),
-        "disable": [
-          function(date) {
-            // return true to disable
-            return (date.getDay() === 0 || date.getDay() === 6);
-          }
-        ],
-        "locale": {
-          "firstDayOfWeek": 1 // start week on Monday
-        },
-    });
-  //   flatpickr(this.maraude_dateTarget, {
-  //     altInput: true,
-  //     altFormat: "F j, Y",
-  //     dateFormat: "Y-m-d",
-  //     inline: true,
-  //     minDate: "today",
-  //     maxDate: new Date().fp_incr(7),
-  //     "disable": [
-  //       function(date) {
-  //         // return true to disable
-  //         return (date.getDay() === 0 || date.getDay() === 6);
-  //       }
-  //     ],
-  //     "locale": {
-  //       "firstDayOfWeek": 1 // start week on Monday
-  //     }
-  // });
+    if (this.pickup_typeTarget.value === "Retrait Ressourcerie") {
+      flatpickr(this.pickup_dateTarget, {
+          altInput: true,
+          altFormat: "F j, Y",
+          dateFormat: "Y-m-d",
+          minDate: "today",
+          maxDate: new Date().fp_incr(8),
+          "disable": [
+            function(date) {
+              // return true to disable
+              return (date.getDay() === 0 || date.getDay() === 6);
+            }
+          ],
+          "locale": {
+            "firstDayOfWeek": 1 // start week on Monday
+          },
+      });
+    } else if (this.pickup_typeTarget.value === "Dépôt Maraude") {
+      flatpickr(this.pickup_dateTarget, {
+          altInput: true,
+          altFormat: "F j, Y",
+          dateFormat: "Y-m-d",
+          minDate: "today",
+          maxDate: new Date().fp_incr(8),
+          "enable": [
+            function(date) {
+              // return true to disable
+              return (date.getDay() === 2 || date.getDay() === 4);
+            }
+          ],
+          "locale": {
+            "firstDayOfWeek": 1 // start week on Monday
+          },
+      });
+    }
   }
 }
