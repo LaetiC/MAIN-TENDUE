3b1108a2db22362fe41cff7dcdd9d3ea5773d79d
Raphaelle PEREZ HORNBORG <raphaelleph@gmail.com>
2023-02-25 17:37:17 +0100

    last fixes before heroku

--- app/javascript/controllers/display_tab_controller.js
+++ app/javascript/controllers/display_tab_controller.js
@@ -3,10 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="display-tab"
 export default class extends Controller {
   static targets = [ "donationTab" ]
-  static values = {
-    displayDonationTab: { type: Boolean, default: false }
-  }
+  // static values = {
+  //   displayDonationTab: { type: Boolean, default: false }
+  // }
   connect() {
-    if (this.displayDonationTabValue) this.donationTabTarget.click()
+    if (this.donationTabTarget.dataset.valuedonation === "true") this.donationTabTarget.click()
   }
 }
