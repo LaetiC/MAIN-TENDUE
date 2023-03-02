b5812d0579949cdf07193b4c13fa9a2933155f5e
Laetitia Commanay <lmc2266@columbia.edu>
2023-03-02 19:58:36 +0100

    changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,9 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 User.create(first_name: "Ressourcerie", last_name: "Le Poulpe", email: "ressourcerielepoulpe@gmail.com", role:"Donateur", password:"azerty", address: "4 Bis Rue d'Oran, 75018 Paris, France")
-User.create(first_name: "Jean Michel", last_name: "Boudin", email: "michmich@gmail.com", role:"Demandeur", password:"azerty")
-User.create(first_name: "Marie", last_name: "Dupont", email: "mariedupont@gmail.com", role:"Donateur", password:"azerty")
+User.create(first_name: "Adrien", last_name: "Boudin", email: "adrien@gmail.com", role:"Demandeur", password:"azerty")
+User.create(first_name: "Margaux", last_name: "Dupont", email: "margaux@gmail.com", role:"Donateur", password:"azerty")
+User.create(first_name: "John", last_name: "Test", email: "john@test.com", role: "Demandeur", password: "azerty")
 
-Chatroom.create(name: "General Donateur")
-Chatroom.create(name: "General Demandeur")
+Chatroom.create(name: "Messagerie Donateur")
+Chatroom.create(name: "Messagerie Demandes")
